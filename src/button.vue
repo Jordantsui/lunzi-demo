949f0c7a5eab6687d9eb028abc0f55532a5847a8
Jordantsui <jiehaocui@foxmail.com>
2019-04-20 23:21:10 +0800

    填写icon和iconPosition属性，并添加click事件，创建button group

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,38 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}"  @click="$emit('click')">
+        <g-icon class="icon" v-if="icon && !loading" :name="icon"/>
+        <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
+        <!--slot上不能加class-->
+    </button>
 </template>
 <script>
-    export default {}
+    export default {
+        // props: ['icon', 'iconPosition']
+        //下面这种传参方法，将参数视为一个对象，可设置默认值和校验器
+        props: {
+            icon: {},
+            loading:{
+                type: Boolean,
+                default: false
+            },
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator (value) {     //参数的校验器，若用户输入的参数不是left或right，则返回false
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
+    }
 </script>
 <style lang="scss">
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
@@ -12,6 +40,10 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex; justify-content: center; align-items: center;
+        /*上面这两个用于按钮内部的元素居中，vertical-align 用于按钮与外部的东西对齐*/
+        /*加入 display: inline 之后，会出现两个按钮上下不对称的情况，此时加上 vertical-align: middle 可破之*/
+        vertical-align: middle;
         &:hover {
             border-color: var(--border-color-hover);
         }
@@ -20,6 +52,23 @@
         }
         &:focus {
             outline: none;
+        }
+        > .content { order: 2; }     /*  >  这个符号表示递进关系*/
+        /*order 是在使用flex布局之后，用以改变元素顺序的，默认是0*/
+        > .icon { order: 1; margin-right: .1em; }
+        &.icon-right {
+            > .content {
+                order: 1;
+            }
+
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .1em;
+            }
+        }
+        .loading{
+            animation: spin 2s infinite linear;
         }
     }
 </style>
---
+++ src/icon.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg class="g-icon">
+        <use :xlink:href="`#i-${name}`"></use>
+        <!--注意icon图标用JS引进之后的挂载方式-->
+    </svg>
+</template>
+<script>
+    export default {
+        props: ['name']
+    }
+</script>
+<style lang="scss">
+    .g-icon {width: 1em; height: 1em; }
+</style>
