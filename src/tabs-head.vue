554601ff9db33be7a20b9a6670265836582aaaba
Jordantsui <jiehaocui@foxmail.com>
2019-05-09 09:58:45 +0800

    tab切换动画完成

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -11,9 +11,11 @@
     export default {
         name: 'GuluTabsHead',
         inject: ['eventBus'],
-        created () {
+        mounted () {
             this.eventBus.$on('update:selected', (item, vm) => {
-                console.log(item)
+                let {width, height, top, left} = vm.$el.getBoundingClientRect()
+                this.$refs.line.style.width = `${width}px`
+                this.$refs.line.style.left = `${left}px`
             })
         }
     }
@@ -25,13 +27,12 @@
         display: flex;       /*加了这一行变成横排*/
         height: $tab-height;
         justify-content: flex-start;
-        border: 1px solid red;
         position: relative;
         > .line {
             position: absolute;
             bottom: 0;
             border-bottom: 1px solid $blue;
-            width: 100px;
+            transition: all 350ms;     /*一开始写的是   left 1s,width 1s  */
         }
         > .actions-wrapper {
             margin-left: auto;      /*把设置按钮移到右边*/
