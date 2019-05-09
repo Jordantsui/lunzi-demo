081eafccfb3c4e9a08dcd0c450a79a5902c2a74b
Jordantsui <jiehaocui@foxmail.com>
2019-05-09 10:17:31 +0800

    完善tabs的样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -23,11 +23,13 @@
 <style scoped lang="scss">
     $tab-height: 40px;
     $blue: blue;
+    $border-color: #ddd;
     .tabs-head {
         display: flex;       /*加了这一行变成横排*/
         height: $tab-height;
         justify-content: flex-start;
         position: relative;
+        border-bottom: 1px solid $border-color;
         > .line {
             position: absolute;
             bottom: 0;
@@ -36,6 +38,10 @@
         }
         > .actions-wrapper {
             margin-left: auto;      /*把设置按钮移到右边*/
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 0 1em;
         }
     }
 </style>
